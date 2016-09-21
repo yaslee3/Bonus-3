c3438268d4adb735feb193bcc6db90d3b5131efb
Yasmine Lee <yaslee3@yahoo.com>
2016-09-21 17:22:01 -0400

    Initial commit comment added

---
+++ src/GuessNumber.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class GuessNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("Welcome to the Guess a Number Game!");
+		System.out.println("++++++++++++++++++++++++++++++++");
+		// prompt user to pick a number between 1 and 100
+		
+		// add 'if' statement 
+		
+		//display messages that indicate whether the user's guess is high or low
+		
+		// display the number of guesses it took the user along with a rating
+		
+		//prompt use to play again
+		
+		//add a loop
+		
+		
+		//add messages that prompt user if way too high or too low
+		
+		
+		// add a try catch to only take number from 1 to 100
+		
+		
+		
+		// add exceptions
+		                   
+	}
+
+}
